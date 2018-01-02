14a7d22c3fc707f18a2d8508904466485f10804d
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-01-02 13:44:37 -0500

    Added comments explaining JSON objects and for loop

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -23,19 +23,22 @@ function loadData() {
     // load nytimes  
     var nytimesUrl ='https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=fe47238bfa9b480891d6a59624521b70';               
     console.log(nytimesUrl);
-
+    // Gets JSON file from NYtimes API
     $.getJSON( nytimesUrl, function( data ) {   
     console.log(data);
         
-
+        //Header for NYtimes article list
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
+        //For loop that goes through the list of headline articles, creates links using the headline
+        // object from inside the JSON object to create the text for links. Also pulls the snippets
+        //object and places below each link. 
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
             $nytElem.append('<li class="article">'+ '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+ '<p>' + article.snippet + '</p>'+ '</li>');
         };
-
+    //If .getJson is unable to retrieve JSON file from NYtimes, show text at headline header
     }).error(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
