47d613651b7ac74bff4eecebc4137709510d5b32
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-02-08 15:54:42 -0500

    Reverted KO.js integration

--- Local-map/js/app.js
+++ Local-map/js/app.js
@@ -88,65 +88,36 @@ function initMap() {
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
 function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    // Clear the infowindow content to give the streetview time to load.
-    infowindow.setContent('');
-    infowindow.marker = marker;
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      infowindow.marker = null;
-    });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
-    // In case the status is OK, which means the pano was found, compute the
-    // position of the streetview image, then calculate the heading, then get a
-    // panorama from that and set the options
-    function getStreetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(
-          nearStreetViewLocation, marker.position);
-          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('pano'), panoramaOptions);
-      } else {
-        infowindow.setContent('<div>' + marker.title + '</div>' +
-          '<div>No Street View Found</div>');
-      }
-    }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    // Open the infowindow on the correct marker.
-    infowindow.open(map, marker);
-  }
+  
 }
+
+
 
 // This function will loop through the markers array and display real-estate.
 function showListings() {
-  hideMarkers(markers);
+  hideMarkers(placeMarkers);
   var bounds = new google.maps.LatLngBounds();
   // Extend the boundaries of the map for each marker and display the marker
   for (var i = 0; i < markers.length; i++) {
     if (markers[i].type == 'real-estate') {
-      markers[i].setMap(map);
-      bounds.extend(markers[i].position);
+      var placesService = new google.maps.places.PlacesService(map);
+        placesService.textSearch({
+          query: markers[i].title,
+          bounds: bounds
+        }, function(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            createMarkersForPlaces(results);
+          }
+        });
+        bounds.extend(myLatLng);      
     }    
   }
   map.fitBounds(bounds);
 }
-
+     
 // This function will loop through the markers array and display restaurants.
 function showRestaurants() {
-  hideMarkers(markers);
+  hideMarkers(placeMarkers);
   var bounds = new google.maps.LatLngBounds();
   // Extend the boundaries of the map for each marker and display the marker
   for (var i = 0; i < markers.length; i++) {
--- Local-map/js/locations.js
+++ Local-map/js/locations.js
@@ -7,7 +7,8 @@
     {type: 'real-estate', title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
     {type: 'real-estate', title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
     {type: 'real-estate', title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}},
-    {type: 'restaurant', title: 'Bogota', location: {lat: 40.678217, lng: -73.978995}},
+    {type: 'restaurant', title: 'Bogota Latin Bistro', location: {lat: 40.678217, lng: -73.978995}},
+    {type: 'restaurant', title: 'Tavern On the Green', location: {lat: 40.678217, lng: -73.978995}},
     {type: 'restaurant', title: 'The Ginger Man', location: {lat: 40.749470, lng: -73.982820}},
     {type: 'museum', title: 'The Metropolitan Museum', location: {lat: 40.779193, lng: -73.962826}},
     {type: 'museum', title: 'Brooklyn Museum', location: {lat: 40.671499, lng: -73.963834}}
