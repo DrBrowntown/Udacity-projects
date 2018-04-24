94d5a74e8bfcae292ea481228d281476458b5f03
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-04-24 09:11:27 -0400

    doc: Add Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Udacity Projects repository 
+
+This contains all the small coding excercises I completed while working through the Fullstack Udacity course.
--- programming-languages/fibonacci-memoization.py
+++ programming-languages/fibonacci-memoization.py
@@ -1,4 +1,4 @@
-#chart caches previous calculations
+# Chart caches previous calculations
 
 chart = {}
 
@@ -6,13 +6,15 @@ def memofibo(n):
     # Check chart for value, return if found
     if n in chart:
         return chart[n]
-            
+    # If n is less than 3 it has a value of 1        
     if n < 3:
         value = 1
+    # Assign recursively called functions to value    
     else:
         value = memofibo(n-1) + memofibo(n-2)
-            
+    # Cache values in chart        
     chart[n] = value
+    # Return value
     return value
     
 print memofibo(500)
